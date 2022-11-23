f2f875d2a4397e6267484876be88f30612c7025d
Gonzalo Rigobello <gonzalo.rigobello18@gmail.com>
2022-11-23 20:38:21 -0300

    routers products

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const cartApi = new Container('cartDB.json');
 
 // permisos de administrador
 
-const esAdmin = true;
+const isAdmin = true;
 
 function notAdmin(route, metod) {
     const error = {
@@ -25,3 +25,38 @@ function notAdmin(route, metod) {
     }
     return error;
 }
+
+function onlyAdmins(req, res, next) {
+    if (!isAdmin) {
+        res.json(notAdmin());
+    } else {
+        next();
+    }
+}
+
+
+// configuro router de productos
+
+const productsRouter = new Router();
+
+productsRouter.get('/', async (req, res) => {
+    const products = await productsApi.listAll();
+    res.json(products);
+})
+
+productsRouter.get('/:id', async (req, res) => {
+    res.json(await productsApi.list(req.params.id));
+})
+
+productsRouter.post('/', onlyAdmins,  async (req, res) => {
+    console.log(req.body);
+    res.json({ id: await productsApi.save(req.body) });
+})
+
+productsRouter.put('/:id', onlyAdmins, async (req, res) => {
+    res.json(await productsApi.update(req.body, req.params.id));
+})
+
+productsRouter.delete('/:id', onlyAdmins, async (req, res) => {
+    res.json(await productsApi.delete(req.params.id));
+})
